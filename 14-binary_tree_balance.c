84a1fa826f18938746ece590fe4e4797f2aa586e
Merch7x <aquamaingi@gmail.com>
2024-02-01 23:14:49 +0300

    Fix linter errors

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -13,7 +13,8 @@ int binary_tree_balance(const binary_tree_t *tree)
 		return (0);
 
 	left_height = tree->left ? 1 + binary_tree_balance(tree->left) : 0;
-        right_height = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
+	right_height = tree->right ? 1 + binary_tree_balance(tree->right) : 0;
 
 	return (left_height - right_height);
 }
+
